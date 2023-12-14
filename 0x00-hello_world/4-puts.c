88131b8029d2edb0f51090ddc75d238ce5fc469d
Obarechrispus <obarechrispus@gmail.com>
2023-12-14 14:03:04 -0800

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>         
-         /**
-	 * main  prints "Programming is like building a multilingual puzzle
-	 * retuns zero value
-	 */
-int main (void) {
-	pus("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
