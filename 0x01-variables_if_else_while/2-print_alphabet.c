2bc6e9863710e8cff2b4befe8f88537412a48461
Obarechrispus <obarechrispus@gmail.com>
2023-12-17 23:37:34 -0800

    1. The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+*Write a program that prints the alphabet in lowercase, followed by a new line.
+*/
+void main ()
+{
+	char c;
+       for (c= 'a'; c<='z'; c++)
+       {
+	       _putchar(c);
+       }
+       _putchar("\n");
+}
