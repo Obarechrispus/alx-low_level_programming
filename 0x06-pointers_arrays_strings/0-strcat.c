9ea77123126cf5341a7b6dfd63b6b946e2022b6e
Obarechrispus <obarechrispus@gmail.com>
2024-01-04 05:27:19 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,28 @@
-d
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_stricat- check the code
+ *@*dest:checks the code
+ *@*src:checks the code
+ *@n: checks 
+ * Return: dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0,b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+	       dest[a] = src [a];
+		a++;
+		b++;
+	}
+	dest [a] = '\0';	
+	return (dest);
+}
