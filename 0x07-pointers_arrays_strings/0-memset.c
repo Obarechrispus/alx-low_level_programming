2a8e7daea594deb33c18f71c9a93ce81b5889923
Obarechrispus <obarechrispus@gmail.com>
2024-01-08 06:21:27 -0800

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,12 +4,12 @@
  *_memset - fill the memory
  *@s: the variable we are changing
  *@b: the constant we are convcerting to
- *@n: the value 
+ *@n: the value
  *Return: (s)
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	_memset(s , b , sizeof(unsigned int)*n);
+	_memset(s, b, sizeof(unsigned int) * n);
 	return (s);
 }
