8d2e05317e39435855d21b59b43a56460405d6cc
Obarechrispus <obarechrispus@gmail.com>
2024-01-08 11:06:32 -0800

    task3&4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+ * _strpbrk - searches
+ * @s: pointer to
+ * @accept: array of bytes
+ * Return: pointe
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
