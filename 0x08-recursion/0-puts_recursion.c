e97e0de2c06e38216bca04d0c485dcda67e818c8
Obarechrispus <obarechrispus@gmail.com>
2024-01-10 10:05:23 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
-*_puts_recursion - the function
-*@s: describes
-*Return: Gives a null return
+ *_puts_recursion - the function
+ *@s: describes
+ *Return: Gives a null return
 */
 
 void _puts_recursion(char *s)
 {
 	_putchar(*s);
-	s++;
-	
 }
