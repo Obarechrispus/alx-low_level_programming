401c3b418aa0189af55101668fe9ff2fff2bb59c
Obarechrispus <obarechrispus@gmail.com>
2024-01-11 00:57:00 -0800

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ *factorial - calculate the factorial
+ *@n: the number
+ *Return:
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
