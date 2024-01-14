e58292b5fc8122667d11f85910dc3d05ff60d442
Obarechrispus <obarechrispus@gmail.com>
2024-01-14 09:18:44 -0800

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ *main - introductio
+ *@argc: count
+ *@argv: vecto
+ *Return: null
+ */
+
 int main (int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
-
