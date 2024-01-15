98dbbc6884b0e2fb06b269dc39d78cbe2b24e8bd
Obarechrispus <obarechrispus@gmail.com>
2024-01-15 01:32:29 -0800

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,25 @@
 #include "main.h"
-#include "stdio.h"
-#include "stdlib.h"
+
+/**
+ * main - program that multip
+ * @argc: numbe
+ * @argv: array
+ *
+ * Return: always 0
+ **/
 
 int main(int argc, char *argv[])
 {
 	int a = 0;
 	int b = 0;
 
-	if (argc ==3)
+	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
-		printf("%d\n", a*b);
+		printf("%d\n", a * b);
 	}
-	else 
+	else
 	{
 		printf("Error\n");
 		return (1);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
-int main (int argc, char *argv[])
+int main (int argc, char *argv[]);
 int main(int argc, char *argv[]);
 
 #endif
