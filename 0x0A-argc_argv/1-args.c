8aec84c6f97eb9599c5fb00122a69f5454ff9fa5
Obarechrispus <obarechrispus@gmail.com>
2024-01-14 23:12:59 -0800

    1. Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,10 +2,11 @@
 
 /**
  *main - prints the number of count
- *argc: counts of argument
- *argv: the string vecto
+ *@argc: counts of argument
+ *@argv: the string vecto
  *Return: null
  */
+
 int main(int argc, char *argv[])
 {
 	(void) argv;
