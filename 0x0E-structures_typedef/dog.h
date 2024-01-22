2e4acba881b713eac0be48ff8fda4ef80e9c38c3
Obarechrispus <obarechrispus@gmail.com>
2024-01-22 13:05:23 -0800

    task2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stddef.h>
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
