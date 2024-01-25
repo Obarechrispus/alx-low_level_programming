36f3c4a29017c203bac0271c8d8abd814bc5d826
Obarechrispus <obarechrispus@gmail.com>
2024-01-25 11:31:59 -0800

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_all - Prints 
+ * print_all - Prints
  * @format: A listtypes
  *          c: c
  *          i: i
@@ -10,32 +10,42 @@
  *          s: char * (if the string is NULL, print (nil) instead)
  *Return: ola
  */
-void print_all(const char * const format, ...) {
+void print_all(const char * const format, ...)
+{
 	char *separator = "";
 	unsigned int i = 0;
-    
 	va_list args;
-    va_start(args, format);
 
-    while (format && format[i]) {
-        if (format[i] == 'c') {
-            printf("%s%c", separator, va_arg(args, int));
-        } else if (format[i] == 'i') {
-            printf("%s%d", separator, va_arg(args, int));
-        } else if (format[i] == 'f') {
-            printf("%s%f", separator, va_arg(args, double));
-        } else if (format[i] == 's') {
-            char *str = va_arg(args, char *);
-            if (str == NULL) {
-                str = "(nil)";
-            }
-            printf("%s%s", separator, str);
-        }
+	va_start(args, format);
 
-        separator = ", ";
-        i++;
-    }
+	while (format && format[i])
+	{
+		if (format[i] == 'c')
+		{
+			printf("%s%c", separator, va_arg(args, int));
+		}
+		else if (format[i] == 'i')
+		{
+			printf("%s%d", separator, va_arg(args, int));
+		} else if (format[i] == 'f')
+		{
+			printf("%s%f", separator, va_arg(args, double));
+		}
+		else if (format[i] == 's')
+		{
+			char *str = va_arg(args, char *);
 
-    va_end(args);
-    printf("\n");
+			if (str == NULL)
+			{
+				str = "(nil)";
+			}
+			printf("%s%s", separator, str);
+		}
+
+		separator = ", ";
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
 }
