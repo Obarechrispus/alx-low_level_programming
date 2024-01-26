004d743ccfb5a4f27c6169183edf8620fc821f5a
Obarechrispus <obarechrispus@gmail.com>
2024-01-26 11:54:34 -0800

    task 3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,25 +1,13 @@
+#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include "variadic_functions.h"
-/**
- * _putchar - writes a character to the standard output
- * @c: character to write
- * Return: number of characters written (1) or -1 on error
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+#include "variadic_function.h"
 /**
  *print_numbers - ...
- *@separator: ...
- *@n: ...
+ *@separator: separator the
+ *@n: the loop to itirate through
  *
- *
- *Return: ...
+ *Return: ..
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
@@ -27,36 +15,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; ++i)
 	{
-		int num = va_arg(args, int);
+		printf("%d", va_arg(args, int));
+	if (separator != NULL && i < n - 1)
+		printf("%s", separator);
 
-		if (i > 0 && separator != NULL)
-		{
-			const char *sep = separator;
-
-			if (sep != NULL)
-				while (*sep != '\0')
-				{
-					_putchar(*sep);
-					sep++;
-				}
-			_putchar(*sep);
-		}
-		if (num < 0)
-		{
-			_putchar('-');
-			num = -num;
-		}
-		if (num == 0)
-			_putchar('0');
-		while (num > 0)
-		{
-			int digit = num % 10;
-
-			_putchar(digit + '0');
-			num /= 10;
-		}
 	}
-	_putchar('\n');
+	va_end(args);
+	printf("\n");
 }
