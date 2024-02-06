ceaef9d7471594fb8916a2a5afc6a06b3effee1b
Obarechrispus <obarechrispus@gmail.com>
2024-02-06 12:06:47 -0800

    task7

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,12 +9,12 @@
  */
 void free_listint(listint_t *head)
 {
-        listint_t *list;
+		listint_t *list;
 
-        while (head != NULL)
-        {
-                list = head;
-                head = head->next;
-                free(list);
-        }
+		while (head != NULL)
+		{
+				list = head;
+				head = head->next;
+				free(list);
+		}
 }
