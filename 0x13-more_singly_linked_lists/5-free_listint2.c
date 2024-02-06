6ee8811ec0c4137b7e29ddb8f798ec2002143da0
Obarechrispus <obarechrispus@gmail.com>
2024-02-06 12:00:43 -0800

    task6

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+    *head = NULL;
+}
+
