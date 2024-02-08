1c8f5ac63165161816acd3641caf9fe66d7476fe
Obarechrispus <obarechrispus@gmail.com>
2024-02-08 09:43:29 -0800

    task0

---
+++ 0-binary_to_uint.c/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *binary_to_uint - ....
+ *@b: ....
+ *
+ *Return: ....
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int number = 0;
+
+	if (b == NULL)
+		return (0);
+	if (*b != '0' && *b != '1')
+	       return (0);
+	while (*b != '\0')
+	{
+	number = (number << 1) + (*b - '0');
+	b++;
+	}
+	return (number);
+}
