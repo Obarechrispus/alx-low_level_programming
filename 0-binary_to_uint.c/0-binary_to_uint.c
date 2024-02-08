2f1575725aa83ee5df75e69f3f769f0baeb25a9d
Obarechrispus <obarechrispus@gmail.com>
2024-02-08 09:56:40 -0800

    tasko

--- 0-binary_to_uint.c/0-binary_to_uint.c
+++ 0-binary_to_uint.c/0-binary_to_uint.c
@@ -9,12 +9,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	int number = 0;
 
-	if (b == NULL)
+	/**
+	 * if (b == NULL)
 		return (0);
 	if (*b != '0' && *b != '1')
-	       return (0);
+				 return (0);
+	*/
 	while (*b != '\0')
 	{
+		 if (b == NULL)
+				return (0);
+				if (*b != '0' && *b != '1')
+		return (0);
 	number = (number << 1) + (*b - '0');
 	b++;
 	}
