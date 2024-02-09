d8aca81fc39d7f6e96aaff0f392c1bcaa4926466
Obarechrispus <obarechrispus@gmail.com>
2024-02-09 02:05:47 -0800

    task3

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - ....
+ *@index
+ *
+ *
+ *
+ *
+ *Return: the index
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int x = 1UL << index;
+	unsigned long int w =( (n & x) ? 1 : 0);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	return (w);
+}
