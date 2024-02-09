765201661e86f16a875bbf0a7d2c15e99263a507
Obarechrispus <obarechrispus@gmail.com>
2024-02-08 22:02:51 -0800

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,25 +8,13 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int number = 0;
-	/**unsigned int k;
-	unsigned int len = strlen(b);
-	
-	 * if (b == NULL)
-		return (0);
-	if (*b != '0' && *b != '1')
-				 return (0);
-	*/
+
 	while (*b != '\0')
 	{
 		if (b == NULL)
 		return (0);
 		if (*b != '0' && *b != '1')
 		return (0);
-		/**for (k = len; k > 0; k++)
-		{
-			if(b[k] != 48 && b[k] != 49)
-			return (0);
-		}*/
 	number = (number << 1) + (*b - '0');
 	b++;
 	}
