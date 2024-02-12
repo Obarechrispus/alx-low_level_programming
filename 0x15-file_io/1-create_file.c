f7a88669c6ba88c1aca4dcce75e239b06dcadb2c
Obarechrispus <obarechrispus@gmail.com>
2024-02-12 07:25:06 -0800

    task2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /**
  *create_file - ...
  *@filename: ....
@@ -18,11 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-
 	while (text_content[i] != '\0')
 	{
 		i++;
-
 	}
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
